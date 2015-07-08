5c1d9a60ba6a21a7f103a8bb270a30657ab52405
asdevelopments <sysadm@dev.as>
2015-07-08 12:45:06 +0200

    Update megacli-adp-discovery.sh

--- files/hwraid-megacli/scripts/megacli-adp-discovery.sh
+++ files/hwraid-megacli/scripts/megacli-adp-discovery.sh
@@ -2,7 +2,7 @@
 # Author: Lesovsky A.V.
 # Adapters auto-discovery via MegaCLI. VERY VERY EXPERIMENTAL (TESTED WITH 8.02.21 Oct 21, 2011)
 
-adp_list=$(sudo megacli adpallinfo aALL nolog |grep "^Adapter #" |cut -d# -f2)
+adp_list=$(megacli adpallinfo aALL nolog |grep "^Adapter #" |cut -d# -f2)
 
 if [[ $1 = raw ]]; then
   for adp in ${adp_list}; do echo $adp; done ; exit 0
